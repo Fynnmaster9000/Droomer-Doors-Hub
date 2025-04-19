48071dd90f4a6e99602091e869a5a3fee9074620
Fynnmaster9000 <fynnmaster9000@gmail.com>
2025-04-19 16:43:22 +0200

    Update Doors-Droomer-Hub.lua

--- Doors-Droomer-Hub.lua
+++ Doors-Droomer-Hub.lua
@@ -37,11 +37,9 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Esp", 4483362458) -- Title, Image
 
-local Toggle = Tab:CreateToggle({
-   Name = "Door ESP",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   Print("Skibidi")
+local Button = Tab:CreateButton({
+   Name = "Sigma Boy",
+   Callback = function()
+  print("Droomer on Top")
    end,
 })
