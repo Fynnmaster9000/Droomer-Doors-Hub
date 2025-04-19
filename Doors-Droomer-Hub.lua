0f15917e5dec1a5ed0ae16be043fccf39cc93d8c
Fynnmaster9000 <fynnmaster9000@gmail.com>
2025-04-19 11:46:34 +0200

    Update Doors-Droomer-Hub.lua

--- Doors-Droomer-Hub.lua
+++ Doors-Droomer-Hub.lua
@@ -30,6 +30,6 @@ local Window = Rayfield:CreateWindow({
        FileName = "Doors93", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Key = {"Doors93"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
